4482459085d75ae879dd7a81867b05c70c63291e
Ivan <techydad05@gmail.com>
2024-03-06 21:03:44 -0500

    Update SideBar.svelte

--- src/lib/components/SideBar.svelte
+++ src/lib/components/SideBar.svelte
@@ -15,7 +15,7 @@
 </button>
 <div use:portalled>
    {#if $open}
-      <div {...$overlay} use:overlay class="fixed inset-0 z-20 bg-black/50" transition:fade={{ duration: 150 }} />
+      <div {...$overlay} use:overlay class="fixed inset-0 z-[100] bg-black/50" transition:fade={{ duration: 150 }} />
       <div {...$content} use:content class="overflow-auto fixed left-0 top-0 z-50 h-screen w-full max-w-[350px] bg-white shadow-lg focus:outline-none" transition:fly={{ x: '-100%', duration: 300, opacity: 1, }}>
          <div class="flex flex-col justify-between h-full text-primary-content bg-secondary p-6">
             <div>
