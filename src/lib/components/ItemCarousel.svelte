7ca063399840fd27a1594f59a87d8b256df13ed9
Ivan <techydad05@gmail.com>
2024-03-20 13:37:08 -0400

    update app.postcss

--- src/lib/components/ItemCarousel.svelte
+++ src/lib/components/ItemCarousel.svelte
@@ -5,7 +5,6 @@
     const Plugins = import("@slidy/plugins");
     export let items = [];
 
-
     const images = [
         {
             src: "mimosa.png",
@@ -28,67 +27,59 @@
     ];
 </script>
 
-<div class="p-4">
-    <!-- find out why plugins arent working in prod build.. might be dom not loaded  -->
-    {#await Plugins}
-        Loading....
-    {:then Plugins}   
-        <Slidy --slidy-thumbnail-size={"100px"}
-            slides={items}
-            counter={false}
-            duration={450}
-            animation={flip}
-            snap="deck"
-            gravity={1.45}
-            loop
-            thumbnail
-            getThumbSrc={(item) => item.thumbnail}
-            plugins={[Plugins.autoplay(), Plugins.log()]}
-            arrows={false}
-            let:item
-        >
-            <div class="card w-96 glass">
-              <figure><img src={item.thumbnail} class="h-[40vw] mt-5" alt={item.title}/></figure>
-              <div class="card-body">
-                <h2 class="card-title">{item.title}</h2>
-                <p>{item.description}</p>
-                <div class="card-actions justify-end">
-                  <button class="btn btn-primary">Add to Cart </button>
+<div class="bg-secondary">
+    <div class="bg-secondary-content">
+        <!-- find out why plugins arent working in prod build.. might be dom not loaded  -->
+        {#await Plugins}
+            Loading....
+        {:then Plugins}
+            <Slidy
+                --slidy-slide-width={"100%"}
+                --slidy-width={"100%"}
+                slides={items}
+                counter={false}
+                duration={450}
+                animation={flip}
+                snap="deck"
+                gravity={1.45}
+                loop
+                plugins={[Plugins.autoplay(), Plugins.log()]}
+                arrows={false}
+                let:item
+            >
+                <div class="card w-96 glass">
+                    <figure>
+                        <img
+                            src={item.thumbnail}
+                            class="h-[40vw] mt-5"
+                            alt={item.title}
+                        />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">{item.title}</h2>
+                        <p>{item.description}</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary"
+                                >Add to Cart
+                            </button>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-        </Slidy>
-    {:catch error}
-        {console.log(error)}
-    {/await}
+            </Slidy>
+        {:catch error}
+            {console.log(error)}
+        {/await}
+    </div>
 </div>
 
 <style>
-    main {
-        --icon-size: calc(2 * var(--font-size-8));
-    }
-
     h2 {
         margin-bottom: var(--size-3);
     }
-
-    main section {
-        padding-block: var(--size-8);
-        display: flex;
-        flex-flow: column;
-        align-items: center;
-        gap: var(--size-2);
-        text-align: center;
-    }
-
-    article {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: var(--size-3);
-        border-radius: var(--size-3);
-    }
     :global(.slidy-slides) {
         justify-content: center;
+    }
+    :global(.slidy-slides, .slidy) {
+        overflow: visible !important;
     }
 </style>
