b496147024aa8c68f5a6dacd46c90c5d126b953c
Ivan <techydad05@gmail.com>
2024-03-22 16:56:21 -0400

    update

--- src/lib/components/AnimatedText.svelte
+++ src/lib/components/AnimatedText.svelte
@@ -1,16 +1,140 @@
 <script>
+    import postcss from "postcss";
+
     export let text = "";
 </script>
-
-<div
-    class="logo-container absolute pl-5 md:pl-0 md:relative"
->
+<!-- <div class="grid"></div>
+<div class="lines"></div> -->
+<div class="absolute right-4 top-[20%] text-8xl">
+    <h1>
+      <span>RETRO</span>
+      <span>RETRO</span>
+    </h1>
+    <h2>Text Effect</h2>
+</div>
+<!-- <div class="logo-container absolute pl-5 md:pl-0 md:relative">
     <div class="line bg-secondary"></div>
-    <h1 id="page-logo" class="text-9xl md:text-[10rem] pl-4 pb-6 opacity-0">
+    <h1
+        id="page-logo"
+        class="shadow1 text-9xl md:text-[10rem] pl-4 pb-6 opacity-0"
+    >
         {text}
     </h1>
-</div>
+    <h1>{text}</h1>
+    <h2>testies</h2>
+</div> -->
+
 <style>
+    @import url("https://fonts.googleapis.com/css2?family=Mr+Dafoe&display=swap");
+    @import url("https://fonts.googleapis.com/css2?family=Exo:wght@900&display=swap");
+
+    .lines {
+        position: fixed;
+        width: 100vw;
+        height: 4em;
+        background: linear-gradient(
+            rgba(89, 193, 254, 0.2) 20%,
+            #59c1fe 40%,
+            #59c1fe 60%,
+            rgba(89, 193, 254, 0.2) 80%
+        );
+        background-size: 1px 0.5em;
+        box-shadow: 0 0 1em rgba(89, 193, 254, 0.4);
+        transform: translateY(-1em);
+        left: 0;
+    }
+
+    h1 {
+        position: relative;
+        font-family: "Exo";
+        font-size: 7rem;
+        margin: 0;
+        transform: skew(-15deg);
+        letter-spacing: 0.03em;
+    }
+    h1::after {
+        content: "";
+        position: absolute;
+        top: -0.1em;
+        right: 0.05em;
+        width: 0.4em;
+        height: 0.4em;
+        background:
+            radial-gradient(
+                white 3%,
+                rgba(255, 255, 255, 0.3) 15%,
+                rgba(255, 255, 255, 0.05) 60%,
+                transparent 80%
+            ),
+            radial-gradient(rgba(255, 255, 255, 0.2) 50%, transparent 60%) 50%
+                50%/5% 100%,
+            radial-gradient(rgba(255, 255, 255, 0.2) 50%, transparent 60%) 50%
+                50%/70% 5%;
+        background-repeat: no-repeat;
+    }
+    h1 span:first-child {
+        display: block;
+        text-shadow:
+            0 0 0.1em #8ba2d0,
+            0 0 0.2em black,
+            0 0 5em #165ff3;
+        -webkit-text-stroke: 0.06em rgba(0, 0, 0, 0.5);
+    }
+    h1 span:last-child {
+        position: absolute;
+        left: 0;
+        top: 0;
+        background-image: linear-gradient(
+            #032d50 25%,
+            #00a1ef 35%,
+            white 50%,
+            #20125f 50%,
+            #8313e7 55%,
+            #ff61af 75%
+        );
+        -webkit-text-stroke: 0.01em #94a0b9;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+
+    h2 {
+        font-family: "Mr Dafoe";
+        margin: 0;
+        font-size: 5.5rem;
+        margin-top: -0.6em;
+        color: white;
+        text-shadow:
+            0 0 0.05em #fff,
+            0 0 0.2em #fe05e1,
+            0 0 0.3em #fe05e1;
+        transform: rotate(-7deg);
+    }
+
+    .grid {
+        background: linear-gradient(
+                transparent 65%,
+                rgba(46, 38, 255, 0.4) 75%,
+                #7d41e6 80%,
+                rgba(46, 38, 255, 0.4) 85%,
+                transparent 95%
+            ),
+            linear-gradient(
+                90deg,
+                transparent 65%,
+                rgba(46, 38, 255, 0.4) 75%,
+                #7d41e6 80%,
+                rgba(46, 38, 255, 0.4) 85%,
+                transparent 95%
+            );
+        background-size: 30px 30px;
+        width: 200vw;
+        height: 300vh;
+        position: absolute;
+        bottom: -120vh;
+        transform: rotateX(-100deg);
+        -webkit-mask-image: linear-gradient(black, rgba(0, 0, 0, 0) 80%);
+    }
+
     .line {
         position: absolute;
         top: calc(100% - 2px);
@@ -21,10 +145,12 @@
         transform: scaleX(0);
     }
     .is-snapped .logo-container .line {
-        animation: line-animation 2.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) infinite alternate;
+        animation: line-animation 2.5s cubic-bezier(0.25, 0.46, 0.45, 0.94)
+            infinite alternate;
     }
     .is-snapped .logo-container h1#page-logo {
-        animation: clip-path-reveal-1 2.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) infinite alternate;
+        animation: clip-path-reveal-1 2.5s cubic-bezier(0.25, 0.46, 0.45, 0.94)
+            infinite alternate;
     }
 
     @keyframes line-animation {
@@ -54,7 +180,7 @@
         }
     }
     @keyframes clip-path-reveal-1 {
-        0%, 
+        0%,
         25% {
             opacity: 1;
             clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0% 100%);
--- src/lib/components/ItemCarousel.svelte
+++ src/lib/components/ItemCarousel.svelte
@@ -4,72 +4,69 @@
     import "@slidy/svelte/dist/slidy.css";
     const Plugins = import("@slidy/plugins");
     export let items = [];
+    let slidyitem = items[0];
 
     const images = [
         {
             src: "mimosa.png",
             width: "90vw",
             height: "405px",
-            figCap: "testies1",
+            title: "testies1",
         },
         {
             src: "volcano.png",
             width: "90vw",
             height: "405px",
-            figCap: "testies2",
+            title: "testies2",
         },
         {
             src: "tikis.png",
             width: "90vw",
             height: "405px",
-            figCap: "testies3",
+            title: "testies3",
         },
     ];
 </script>
 
-<div class="bg-secondary">
-    <div class="bg-secondary-content">
-        <!-- find out why plugins arent working in prod build.. might be dom not loaded  -->
-        {#await Plugins}
-            Loading....
-        {:then Plugins}
-            <Slidy
-                --slidy-slide-width={"100%"}
-                --slidy-width={"100%"}
-                slides={items}
-                counter={false}
-                duration={450}
-                animation={flip}
-                snap="deck"
-                gravity={1.45}
-                loop
-                plugins={[Plugins.autoplay(), Plugins.log()]}
-                arrows={false}
-                let:item
-            >
-                <div class="card w-96 glass">
-                    <figure>
-                        <img
-                            src={item.thumbnail}
-                            class="h-[40vw] mt-5"
-                            alt={item.title}
-                        />
-                    </figure>
-                    <div class="card-body">
-                        <h2 class="card-title">{item.title}</h2>
-                        <p>{item.description}</p>
-                        <div class="card-actions justify-end">
-                            <button class="btn btn-primary"
-                                >Add to Cart
-                            </button>
-                        </div>
-                    </div>
+<div class="bg-secondary p-4">
+    <!-- find out why plugins arent working in prod build.. might be dom not loaded  -->
+    {#await Plugins}
+        Loading....
+    {:then Plugins}
+        <div class="card card-side bg-base-100 shadow-xl flex-col">
+            <figure class="!justify-start flex-1">
+                <Slidy
+                    on:index={(e) => (slidyitem = items[e.detail.index])}
+                    getImgSrc={(item) => item.thumbnail}
+                    --slidy-slide-width={"100%"}
+                    --slidy-width={"100%"}
+                    slides={items}
+                    counter={false}
+                    duration={450}
+                    animation={flip}
+                    snap="deck"
+                    gravity={1.45}
+                    loop
+                    plugins={[Plugins.autoplay(), Plugins.log()]}
+                    arrows={false}
+                    let:item
+                />
+            </figure>
+            <div class="card-body flex-1 flex-col px-4 py-0">
+                <h2 class="card-title text-5xl leading-none">{slidyitem?.title}</h2>
+                <p class="text-2xl">{slidyitem?.description}</p>
+                <div class="card-actions justify-end p-4">
+                    <button
+                        on:click={() => location.assign(location.pathname)}
+                        class="btn btn-primary">Go to</button
+                    >
                 </div>
-            </Slidy>
-        {:catch error}
-            {console.log(error)}
-        {/await}
-    </div>
+            </div>
+        </div>
+        <!-- {JSON.stringify(slidyitem,null, 2)} -->
+    {:catch error}
+        {console.log(error)}
+    {/await}
 </div>
 
 <style>
@@ -79,7 +76,34 @@
     :global(.slidy-slides) {
         justify-content: center;
     }
+    :global(.slidy) {
+        display: inline-grid;
+    }
     :global(.slidy-slides, .slidy) {
         overflow: visible !important;
+    }
+    /* learn how to use grid layout */
+    .info-section {
+        min-height: fit-content;
+        vertical-align: middle;
+        display: inline-grid;
+        grid-auto-columns: 1fr;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 1fr 1fr 1fr;
+        gap: 0px 0px;
+        grid-template-areas:
+            ". title ."
+            ". desc ."
+            ". button .";
+    }
+    .title {
+        grid-area: title;
+    }
+    .desc {
+        grid-area: desc;
+        grid-column: span 3;
+    }
+    .button {
+        grid-area: button;
     }
 </style>
