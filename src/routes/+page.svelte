c0f609f39472581a803e39be07d68fe69a47a146
Ivan <techydad05@gmail.com>
2024-04-03 00:10:20 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,7 +24,7 @@
 <svg xmlns='http://www.w3.org/2000/svg' class="fill-primary bg-base-100" viewBox='0 0 1185 248'><circle cx='76' cy='121.1' r='20' class='a'/><circle cx='870' cy='201.1' r='11' class='a'/><circle cx='814.5' cy='165.6' r='24.5' class='a'/><path d='M0 0v17.7c22.7 14.8 53 31.9 90.7 51.5 150.8 78 322 116.6 424.8 69.3 102.9-47.4 138-69.3 210.8-69.3s118.3 48.6 219.5 38.3 76.3-59.3 188.7-59.3c18.9 0 35.5 2.6 50.5 6.8V0H0z' class='a'/></svg>
 
 <style>
-   .shark {
+   /* .shark {
       -webkit-animation: shark-swim linear;
       animation: shark-swim linear;
       -webkit-animation-timeline: view();
@@ -39,7 +39,7 @@
          transform: translate3d(80vw, -80px, 500px);
          -webkit-transform: translate3d(80vw, -80px, 500px);
       }
-   }
+   } */
 </style>
 
 <!-- <Collection products={data.products} /> -->
