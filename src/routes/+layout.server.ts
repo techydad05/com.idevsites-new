14fd6b45d865e01ddabf7a14586cdce9257eaf3e
Ivan <techydad05@gmail.com>
2024-05-19 13:53:49 -0400

    working on auth

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,6 @@
 import type { LayoutServerLoad } from './$types'
 
 export const load: LayoutServerLoad = async function ({ locals }) {
-   console.log("locals in layout.server:", locals);
    return {
       // locals.user and locals.cart are set in hooks.server.js
       user: locals.user, 
